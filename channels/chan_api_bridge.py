8e3266d80d3f9aa1bcbb01795a3679e26da4ec48
Rose22 <rosemelody22@gmail.com>
2026-03-01 20:15:29 +0100

    oops i broke it

--- channels/chan_api_bridge.py
+++ channels/chan_api_bridge.py
@@ -1,3 +1,5 @@
+import core
+
 class ApiChannel(core.channel.Channel):
     """
     OpenAI API endpoint. allows you to connect any AI app or webui to opticlaw, and then opticlaw to any llm backend such as llamacpp or koboldcpp, to easily grant agent functionality to any AI app.
