45fda901f102c757d2f5f960cd218d15457d5e7a
Rose22 <rosemelody22@gmail.com>
2026-07-23 16:30:10 +0200

    [core] fix message injections getting injected into assistant messages

--- core/chat.py
+++ core/chat.py
@@ -440,17 +440,18 @@ class Chat:
 
         # inject any special messages coming from on_message_inject() in modules, such as timestamps
         injections = []
-        for module_name, module in self.channel.manager.modules.items():
-            if hasattr(module, 'on_message_inject'):
-                try:
-                    injection = await module.on_message_inject()
-                    if injection:
-                        injections.append(injection)
-                except Exception as e:
-                    self.channel.log("module error", f"{module.name}: in on_message_inject(): {core.detail_error(e)}")
+        if message.get("role") == "user":
+            for module_name, module in self.channel.manager.modules.items():
+                if hasattr(module, 'on_message_inject'):
+                    try:
+                        injection = await module.on_message_inject()
+                        if injection:
+                            injections.append(injection)
+                    except Exception as e:
+                        self.channel.log("module error", f"{module.name}: in on_message_inject(): {core.detail_error(e)}")
 
-        if injections:
-            new_message["injection"] = "\n\n".join(injections)
+            if injections:
+                new_message["injection"] = "\n\n".join(injections)
 
         self.data[self.current]["messages"].append(new_message)
 
