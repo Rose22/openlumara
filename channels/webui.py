97a88c632efa22bad5460faa45efa3b2fc37d1f1
Som-anon <106993652+Som-anon@users.noreply.github.com>
2026-08-13 17:26:30 +0200

    Update webui.py
    Prevent duplicates in the model list.
    The webui has problems rendering model lists if there are duplicates

--- channels/webui.py
+++ channels/webui.py
@@ -584,6 +584,8 @@ async def create_fastapi(channel):
     async def models_get():
         """Returns a list of all available AI models"""
         result = await channel.manager.API.list_models()
+        result = list(set(result))
+        
         if isinstance(result, core.api.APIError):
             return api_result(str(result), success=False)
 
